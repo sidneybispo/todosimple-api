ea2992ea442f16b30183be1d1fd5441b7d8711df
sidneybispo <protecney@gmail.com>
2022-10-14 14:54:16 -0300

    Spring Boot project

---
+++ src/test/java/com/sidneydev/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidneydev.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
